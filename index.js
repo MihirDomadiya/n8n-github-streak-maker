9097727a4201a76e5263894d45187e915bb2c5a1
MihirDomadiya <mihirdomadiya55@gmail.com>
2025-08-08 20:44:36 +0530

    MD

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ import simpleGit from "simple-git";
 const path = "./data.json";
 
 // Target date
-const date = moment("2025-07-28").format(); // or use toISOString()
+const date = moment("2025-08-05").format(); // or use toISOString()
 // YYYY-MM-DD
 const data = { date };
 
