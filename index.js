519c7ae111989002ea4ca1fe3dbbaa7b8475bd87
MihirDomadiya <mihirdomadiya55@gmail.com>
2025-12-31 21:34:32 +0530

    aa

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ import simpleGit from "simple-git";
 const path = "./data.json";
 
 // Target date
-const date = moment("2025-12-21").format();
+const date = moment("2025-12-22").format();
 // YYYY-MM-DD
 const data = { date };
 
